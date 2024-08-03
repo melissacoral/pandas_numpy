4fded1046340a50291c8b089c28a21f746f59b72
Melissa <melissacoralflorez9118@gmail.com>
2024-08-03 12:02:42 -0500

    abordamos tipos de datos en Numpy

---
+++ tipo_de_datos.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+#arr = np.array([1,2,3,4], dtype='float64') ##definiendo un array con una lista
+#print(arr) ### y pornerlo como flotante y no como entero
+
+#arr.dtype  ## qué tipo de dato tiene numpy
+#print(arr.dtype)
+#arr = np.array([1,2,3,4])
+#print(arr)
+#arr = arr.astype(np.float64)
+#print(arr)
+#arr = np.array([0,1,2,3,4])
+#arr = arr.astype(np.bool_) ### nos lo muestra como entero
+#print(arr)
+#arr = np.array([0,1,2,3,4])
+#arr = arr.astype(np.str_) ### np.str como texto
+#print(arr)
+#arr = np.array(['0','1','2','3','4'])
+#arr = arr.astype(np.int8) ### ahora pasamos de texto a entero
+#print(arr)
